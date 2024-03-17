748e0c183e328a0d0ed3d082663ddd88a641a298
commandos135 <30777946+commandos135@users.noreply.github.com>
2024-03-17 23:03:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 #ezzez
@@ -11,8 +13,18 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "gLFyf"))
+)
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
 input_element.send_keys("real rap pro" + Keys.ENTER)
+
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Real")
+link.click()
+
 
 time.sleep(10)
 
